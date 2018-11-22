034999d11099b82633a2b22ba66f0e051a28e8ee
Michael A. Volz <mvolz@redmuffin.de>
2018-11-22 18:19:30 +0100

    Changed bootstrap to foundation

---
+++ src/OpenGraphTilemaker.Web.Client/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿window.showAlert = (data) => {
+    alert(data);
+
+    return true;
+};
+
+window.say = (data) => {
+    console.dir(data);
+
+    return true;
+};
+
+window.getWindowsWidth = () => {
+    return window.innerWidth;
+};
