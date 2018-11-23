0ec494ee2b8e8e94c0fcf324cc8d5227aedec838
Michael A. Volz <mvolz@redmuffin.de>
2018-11-23 09:40:13 +0100

    Added realtime windowresize statechange

--- src/OpenGraphTilemaker.Web.Client/Features/_Misc/BlazorComponentStateful.cs
+++ src/OpenGraphTilemaker.Web.Client/Features/_Misc/BlazorComponentStateful.cs
@@ -29,17 +29,26 @@ namespace OpenGraphTilemaker.Web.Client.Features
 
         public int WindowWidth { get; set; }
 
+        [JSInvokable]
+        public Task<string> WindowResizedAsync(int windowWidth) {
+            Console.WriteLine($"Window resized: new width: '{windowWidth}'!");
+            WindowWidth = windowWidth;
+            StateHasChanged();
+            return Task.FromResult($"new WindowWidth: {windowWidth}");
+        }
+
         protected async Task RequestAsync<T>(IRequest<T> request) {
             await Time.ThisAsync(async () => await Mediator.Send(request), request.GetType().Name);
         }
 
-        protected async override Task OnParametersSetAsync() {
+        protected override async Task OnParametersSetAsync() {
+            await JSRuntime.Current.InvokeAsync<object>("blazorDemo.initialize", new DotNetObjectRef(this));
             await GetWindowWidth();
             base.OnParametersSet();
         }
 
         public async Task<int> GetWindowWidth() {
-            WindowWidth = await JSRuntime.Current.InvokeAsync<int>("getWindowsWidth");
+            WindowWidth = await JSRuntime.Current.InvokeAsync<int>("blazorDemo.getWindowsWidth");
             StateHasChanged();
             return WindowWidth;
         }
--- src/OpenGraphTilemaker.Web.Client/wwwroot/js/site.js
+++ src/OpenGraphTilemaker.Web.Client/wwwroot/js/site.js
@@ -1,15 +1,55 @@
-﻿window.showAlert = (data) => {
-    alert(data);
+﻿window.blazorDemo = {
+    showAlert: (data) => {
+        alert(data);
 
-    return true;
-};
+        return true;
+    },
 
-window.say = (data) => {
-    console.dir(data);
+    say: (data) => {
+        console.dir(data);
 
-    return true;
-};
+        return true;
+    },
 
-window.getWindowsWidth = () => {
-    return window.innerWidth;
+    getWindowsWidth: () => {
+        return window.innerWidth;
+    },
+
+    focusElement: function(element) {
+        element.focus();
+    },
+
+    initialize: (dotnetClassInstance) => {
+        console.log("JS initialize start!");
+
+        var leftMouseButtonOnlyDown = false;
+
+        function setLeftButtonState(e) {
+            leftMouseButtonOnlyDown = e.buttons === undefined
+                ? e.which === 1
+                : e.buttons === 1;
+        }
+
+        document.body.onmousedown = setLeftButtonState;
+        document.body.onmousemove = setLeftButtonState;
+        document.body.onmouseup = setLeftButtonState;
+
+        window.addEventListener("resize",
+            function() {
+                if (typeof window.resizeTimer !== "undefined") {
+                    clearTimeout(window.resizeTimer);
+                }
+
+                window.resizeTimer = setTimeout(function() {
+                        // Run code here, resizing has "stopped"
+                        dotnetClassInstance.invokeMethodAsync("WindowResizedAsync", window.innerWidth)
+                            .then(data => {
+                                console.log(data);
+                            });
+                    },
+                    250);
+            });
+
+        console.log("JS initialize finished!");
+    }
 };
