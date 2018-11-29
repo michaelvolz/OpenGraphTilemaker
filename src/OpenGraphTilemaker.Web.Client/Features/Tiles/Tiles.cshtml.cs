34e192d50f7a376e5c6146b68d433fb148165c4a
Michael A. Volz <mvolz@redmuffin.de>
2018-11-29 18:35:46 +0100

    Optimized Tiles

--- src/OpenGraphTilemaker.Web.Client/Features/Tiles/Tiles.cshtml.cs
+++ src/OpenGraphTilemaker.Web.Client/Features/Tiles/Tiles.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Common;
 using Microsoft.AspNetCore.Blazor.RenderTree;
+using Microsoft.Extensions.Logging;
 using OpenGraphTilemaker.OpenGraph;
 
 // ReSharper disable MemberCanBePrivate.Global
@@ -12,16 +13,34 @@ namespace OpenGraphTilemaker.Web.Client.Features.Tiles
 {
     public class TilesModel : BlazorComponentStateful
     {
+        private const int OneSecondInMilliseconds = 1000;
+        private readonly string _logPrefix;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TileModel" /> class.
+        /// </summary>
+        protected TilesModel() => _logPrefix = $"### {nameof(TileModel)}";
+
         protected TilesState State => Store.GetState<TilesState>();
-        protected bool IsLoading { get; set; } = true;
+
+        protected bool Loading() => !State.CurrentTiles.Any() && IsLoading;
+        protected bool Empty() => State.CurrentTiles.Any() || IsLoading;
+        protected bool Any() => State.CurrentTiles.Any();
 
         /// <summary>
         ///     OnInit.
         /// </summary>
         protected override async Task OnInitAsync() {
+            Log.LogInformation($"{_logPrefix}.{nameof(OnInitAsync)}");
+
             if (!State.OriginalTiles.Any()) {
+                Log.LogInformation($"{_logPrefix}.{nameof(OnInitAsync)} loading data...");
+
                 IsLoading = true;
+                // for testing purposes only!
+                await Task.Delay(5 * OneSecondInMilliseconds);
                 await RequestAsync(new InitializeTilesRequest());
+
                 StateHasChanged();
             }
 
@@ -32,26 +51,25 @@ namespace OpenGraphTilemaker.Web.Client.Features.Tiles
         ///     BuildRenderTree.
         /// </summary>
         protected override void BuildRenderTree(RenderTreeBuilder builder) {
-            UpdateIfChangeHappenedWithoutCustomEvent();
+            // Fires when 'enter' was pressed in the searchBox  or  searchBox -> blur
+            SearchIfUpdatedAsync().GetAwaiter().GetResult();
 
             base.BuildRenderTree(builder);
         }
 
-        protected async Task OnSortPropertyButtonClick() {
+        protected async Task OnSortProperty() {
             var sortProperty = State.SortProperty != nameof(OpenGraphMetadata.Title)
                 ? nameof(OpenGraphMetadata.Title)
                 : nameof(OpenGraphMetadata.BookmarkTime);
             await RequestAsync(new SortTilesRequest { SortProperty = sortProperty });
         }
 
-        protected async Task OnSortOrderButtonClick() {
+        protected async Task OnSortOrder() {
             var sortOrder = State.SortOrder == SortOrder.Ascending ? SortOrder.Descending : SortOrder.Ascending;
             await RequestAsync(new SortTilesRequest { SortOrder = sortOrder });
         }
 
-        protected async Task OnSearchButtonClick() => await SearchIfUpdatedAsync();
-
-        private void UpdateIfChangeHappenedWithoutCustomEvent() => SearchIfUpdatedAsync().GetAwaiter().GetResult();
+        protected async Task OnSearch() => await SearchIfUpdatedAsync();
 
         private async Task SearchIfUpdatedAsync() {
             if (State.LastSearchText != State.SearchText) {
--- src/OpenGraphTilemaker.Web.Client/Features/_Misc/BlazorComponentStateful.cs
+++ src/OpenGraphTilemaker.Web.Client/Features/_Misc/BlazorComponentStateful.cs
@@ -7,27 +7,36 @@ using Microsoft.AspNetCore.Blazor.Components;
 using Microsoft.AspNetCore.Blazor.Services;
 using Microsoft.Extensions.Logging;
 
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Local
+// ReSharper disable UnusedMember.Global
+
 namespace OpenGraphTilemaker.Web.Client.Features
 {
     public class BlazorComponentStateful : BlazorComponent
     {
         private readonly Lazy<ILogger<BlazorComponentStateful>> _lazyLogger;
 
-        protected BlazorComponentStateful() {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BlazorComponentStateful" /> class.
+        /// </summary>
+        protected BlazorComponentStateful() =>
             _lazyLogger = new Lazy<ILogger<BlazorComponentStateful>>(() => LoggerFactory.CreateLogger<BlazorComponentStateful>());
-        }
 
-        [Inject] public Time Time { get; set; }
-        [Inject] public ILoggerFactory LoggerFactory { get; set; }
+        [Inject] private ILoggerFactory LoggerFactory { get; set; }
+        [Inject] private IMediator Mediator { get; set; }
 
-        public ILogger<BlazorComponentStateful> Log => _lazyLogger.Value;
+        [Inject] protected Time Time { get; set; }
+        [Inject] protected IStore Store { get; set; }
+        [Inject] protected IUriHelper UriHelper { get; set; }
 
-        [Inject] public IMediator Mediator { get; set; }
-        [Inject] public IStore Store { get; set; }
-        [Inject] public IUriHelper UriHelper { get; set; }
+        protected ILogger<BlazorComponentStateful> Log => _lazyLogger.Value;
 
-        protected async Task RequestAsync<T>(IRequest<T> request) {
-            await Mediator.Send(request);
-        }
+        protected bool IsLoading { get; set; } = true;
+
+        protected string HideIf(Func<bool> predicate) => predicate() ? "collapsed" : null;
+        protected string ShowIf(Func<bool> predicate) => predicate() ? null : "collapsed";
+
+        protected async Task RequestAsync<T>(IRequest<T> request) => await Mediator.Send(request);
     }
 }
