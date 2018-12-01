198c857ffdaf904f3b5f6f52a2c6c2505f92b101
Michael A. Volz <mvolz@redmuffin.de>
2018-12-02 00:19:50 +0100

    Added PingPong test for MediatR

---
+++ src/OpenGraphTilemaker.Web.Client/Features/_Misc/PingPong.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+public class Ping : IRequest<string> { }
+
+public class PingHandler : IRequestHandler<Ping, string>
+{
+    public Task<string> Handle(Ping request, CancellationToken cancellationToken) => Task.FromResult("Pong");
+}
