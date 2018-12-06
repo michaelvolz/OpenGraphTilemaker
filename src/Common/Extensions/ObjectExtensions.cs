5fdb5c9800f03573932e666fa37f522d82ebd100
Michael A. Volz <mvolz@redmuffin.de>
2018-12-06 11:46:50 +0100

    Minor refactoring

--- src/Common/Extensions/ObjectExtensions.cs
+++ src/Common/Extensions/ObjectExtensions.cs
@@ -1,75 +1,51 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
+// ReSharper disable UnusedMember.Global
+
 namespace Common.Extensions
 {
     public static class ObjectExtensions
     {
-        public static string ToNullSafeString(this object value)
-        {
-            return value?.ToString();
-        }
-
-        public static string ReturnDump(this object obj)
-        {
+        public static string ReturnDump(this object subject) {
             var settings =
-                new JsonSerializerSettings
-                {
+                new JsonSerializerSettings {
                     ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
                     Formatting = Formatting.Indented
                 };
 
-            return JsonConvert.SerializeObject(obj, settings);
+            return JsonConvert.SerializeObject(subject, settings);
         }
 
-        public static string ReturnDumpFlat(this object obj)
-        {
+        public static string ReturnDumpFlat(this object subject) {
             var settings =
-                new JsonSerializerSettings
-                {
+                new JsonSerializerSettings {
                     ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
                     Formatting = Formatting.None
                 };
 
-            return JsonConvert.SerializeObject(obj, settings);
+            return JsonConvert.SerializeObject(subject, settings);
         }
 
-        // http://stackoverflow.com/questions/9210428/how-to-convert-class-into-dictionarystring-string
-        public static Dictionary<string, dynamic> PropertiesToDictionary(this object obj)
-        {
-            Contract.Requires(obj != null);
-
-            return obj.GetType()
+        public static Dictionary<string, dynamic> PropertiesToDictionary(this object subject) =>
+            subject.GetType()
                 .GetProperties(BindingFlags.Instance | BindingFlags.Public)
-                .ToDictionary(prop => prop.Name, prop => prop.GetValue(obj, null));
-        }
+                .ToDictionary(prop => prop.Name, prop => prop.GetValue(subject, null));
 
-        public static PropertyInfo PublicProperty(this object obj, string propertyName)
-        {
-            Contract.Requires(obj != null);
-            Contract.Requires(!string.IsNullOrEmpty(propertyName));
+        public static PropertyInfo PublicProperty(this object subject, string propertyName) =>
+            subject.GetType().GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
 
-            return obj.GetType().GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
-        }
+        public static IEnumerable<PropertyInfo> PublicProperties(this object subject) =>
+            subject.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
 
-        public static IEnumerable<PropertyInfo> PublicProperties(this object obj)
-        {
-            Contract.Requires(obj != null);
-
-            return obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
-        }
-
-        public static string JSONSerialize(this object obj)
-        {
+        public static string JSONSerialize(this object subject) {
             var settings =
-                new JsonSerializerSettings
-                {
+                new JsonSerializerSettings {
                     ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
                     Formatting = Formatting.None,
                     TypeNameHandling = TypeNameHandling.Arrays
@@ -77,29 +53,26 @@ namespace Common.Extensions
 
             string serializeObject;
 
-            try
-            {
-                serializeObject = JsonConvert.SerializeObject(obj, settings);
+            try {
+                serializeObject = JsonConvert.SerializeObject(subject, settings);
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 throw new InvalidOperationException(
-                    $"JSON serialization failed for object of type: {obj.GetType()}.\r\n{e.Message}", e);
+                    $"JSON serialization failed for object of type: {subject.GetType()}.\r\n{e.Message}", e);
             }
 
             return serializeObject;
         }
 
-        public static T JSONUnserialize<T>(this string stringObj)
-        {
-            var unserialize = (T) JsonConvert.DeserializeObject(stringObj, typeof(T));
+        public static T JSONUnserialize<T>(this string stringObj) {
+            var unserialize = (T)JsonConvert.DeserializeObject(stringObj, typeof(T));
             var uIsList = unserialize.GetType().IsGenericType && unserialize is IEnumerable;
 
             // -- performance optimization
             if (uIsList)
                 return unserialize;
 
-            var item = (T) Activator.CreateInstance(typeof(T));
+            var item = (T)Activator.CreateInstance(typeof(T));
             var iIsList = item.GetType().IsGenericType && item is IEnumerable;
 
             if (!iIsList)
@@ -107,7 +80,7 @@ namespace Common.Extensions
 
             //_log.Warn("JSON Object was not deserialized as 'List', now trying special algorithm!");
 
-            return (T) JsonConvert.DeserializeObject(stringObj, typeof(T), new FlexibleCollectionConverter());
+            return (T)JsonConvert.DeserializeObject(stringObj, typeof(T), new FlexibleCollectionConverter());
         }
 
         /// <summary>
@@ -115,14 +88,10 @@ namespace Common.Extensions
         /// </summary>
         private class FlexibleCollectionConverter : JsonConverter
         {
-            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-            {
-                serializer.Serialize(writer, value);
-            }
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) => serializer.Serialize(writer, value);
 
             public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
-                JsonSerializer serializer)
-            {
+                JsonSerializer serializer) {
                 if (reader.TokenType == JsonToken.StartArray)
                     return serializer.Deserialize(reader, objectType);
 
@@ -130,10 +99,7 @@ namespace Common.Extensions
                 return array.ToObject(objectType);
             }
 
-            public override bool CanConvert(Type objectType)
-            {
-                return typeof(IEnumerable).IsAssignableFrom(objectType);
-            }
+            public override bool CanConvert(Type objectType) => typeof(IEnumerable).IsAssignableFrom(objectType);
         }
 
         /// <summary>
@@ -143,27 +109,18 @@ namespace Common.Extensions
         private class GenericListCreationJsonConverter<T> : JsonConverter
         {
             public override bool CanRead => true;
-
             public override bool CanWrite => false;
-
-            public override bool CanConvert(Type objectType)
-            {
-                return true;
-            }
+            public override bool CanConvert(Type objectType) => true;
 
             public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
-                JsonSerializer serializer)
-            {
+                JsonSerializer serializer) {
                 if (reader.TokenType == JsonToken.StartArray)
                     return serializer.Deserialize<List<T>>(reader);
                 var t = serializer.Deserialize<T>(reader);
-                return new List<T>(new[] {t});
+                return new List<T>(new[] { t });
             }
 
-            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-            {
-                throw new NotImplementedException();
-            }
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) => throw new NotImplementedException();
         }
     }
 }
