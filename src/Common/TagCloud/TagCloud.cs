e4ffd2ef63d502ae7ff26336501194d88cbfd113
Michael A. Volz <mvolz@redmuffin.de>
2018-12-26 17:27:22 +0100

    Add params[] and distinct

--- src/Common/TagCloud/TagCloud.cs
+++ src/Common/TagCloud/TagCloud.cs
@@ -2,41 +2,52 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using Common.Extensions;
+using static Common.Extensions.AssemblyExtensions;
 
 namespace Common.TagCloud
 {
     public class TagCloud
     {
         private const string MySQLMyISAMText = @"TagCloud\mysql_myisam.txt";
+        private const string Space = " ";
         private static string[] _stopWords;
-        private readonly string _location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
         public Dictionary<string, int> Cloud { get; } = new Dictionary<string, int>();
 
-        private static string NormalizedWord(string word) => word.TrimEnd('.').Replace("’", "'");
+        private static string CombineTexts(string[] texts) => texts.Aggregate(string.Empty, (current, next) => $"{current}{Space}{next}");
 
-        public async Task InsertAsync(string text) {
-            await Task.FromResult(0); // async placeholder
+        private static IEnumerable<string> ExtractWords(string text) => text.ToLowerInvariant().RemoveNumbers().Split().Distinct();
 
-            if (_stopWords == null) _stopWords = File.ReadAllLines($@"{_location}\{MySQLMyISAMText}");
+        private static string NormalizeWord(string word) => word.TrimEnd('.').Replace("’", "'");
 
-            foreach (var word in text.RemoveNumbers().Split())
+        /// <summary>
+        ///     Multiple texts inserted at the same time will only add each word once.
+        /// </summary>
+        /// <param name="texts">Text to use.</param>
+        /// <returns>
+        ///     <see cref="Task" />
+        /// </returns>
+        public async Task InsertAsync(params string[] texts) {
+            await Task.FromResult(0); // async placeholder, File.ReadAllLines has no async in netstandard2.0
+
+            if (_stopWords == null) _stopWords = File.ReadAllLines($@"{AssemblyLocation}\{MySQLMyISAMText}");
+
+            foreach (var word in ExtractWords(CombineTexts(texts)))
                 InsertWord(word);
         }
 
         private void InsertWord(string word) {
-            var normalizedWord = NormalizedWord(word);
+            var normalizedWord = NormalizeWord(word);
 
             if (normalizedWord.Length < 2) return;
             if (_stopWords.Contains(normalizedWord, StringComparer.InvariantCultureIgnoreCase)) return;
 
-            var lowerCaseWord = normalizedWord.ToLowerInvariant();
-            if (Cloud.ContainsKey(lowerCaseWord))
-                Cloud[lowerCaseWord] += 1;
+            if (Cloud.ContainsKey(normalizedWord))
+                Cloud[normalizedWord] += 1;
             else
-                Cloud.Add(lowerCaseWord, 1);
+                Cloud.Add(normalizedWord, 1);
         }
     }
 }
