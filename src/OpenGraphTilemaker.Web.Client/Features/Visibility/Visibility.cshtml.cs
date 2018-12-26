bcbaca413cb774c2977fa0c070c6e504d3d9e7fe
Michael A. Volz <mvolz@redmuffin.de>
2018-12-26 18:38:21 +0100

    Add Visibilty component

---
+++ src/OpenGraphTilemaker.Web.Client/Features/Visibility/Visibility.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace OpenGraphTilemaker.Web.Client.Features.ToggleVisibility
+{
+    public class VisibilityModel : BlazorComponent
+    {
+        [Parameter] protected RenderFragment ChildContent { get; set; }
+        [Parameter] protected VisibilityState State { get; set; } = VisibilityState.Undefined;
+    }
+
+    public enum VisibilityState
+    {
+        Undefined = 0,
+        Show = 1,
+        Hide = 2,
+    }
+}
