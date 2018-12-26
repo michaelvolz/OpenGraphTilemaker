e8bcea7c768e0ede85fc0d1b1c6015635d91183a
Michael A. Volz <mvolz@redmuffin.de>
2018-12-26 17:27:50 +0100

    Fix access modifier

--- src/OpenGraphTilemaker.Web.Client/Features/CryptoWatch/CryptoWatchCard.cshtml.cs
+++ src/OpenGraphTilemaker.Web.Client/Features/CryptoWatch/CryptoWatchCard.cshtml.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Blazor.Components;
+﻿using Common;
+using Microsoft.AspNetCore.Blazor.Components;
 
 namespace OpenGraphTilemaker.Web.Client.Features.CryptoWatch
 {
     public class CryptoWatchCardModel : BlazorComponentStateful<CryptoWatchCardModel>
     {
-        [Parameter] private protected CryptoWatchCardData Data { get; set; }
+        [IoC] [Parameter] protected CryptoWatchCardData Data { get; set; }
     }
 }
