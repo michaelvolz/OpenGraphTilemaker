72c6f27021be7173908b36697c05bdfd8e41beae
Michael A. Volz <mvolz@redmuffin.de>
2018-12-26 17:25:31 +0100

    Add AssemblyExtensions

---
+++ src/Common/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Common.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static readonly string AssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    }
+}
