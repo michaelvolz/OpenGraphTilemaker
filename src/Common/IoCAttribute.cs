4d844083b5c4ba9efd398c01f434f976d83e99cc
Michael A. Volz <mvolz@redmuffin.de>
2018-12-26 17:26:06 +0100

    Add IoCAttribute usage for properties

--- src/Common/IoCAttribute.cs
+++ src/Common/IoCAttribute.cs
@@ -8,7 +8,8 @@ namespace Common
     ///     Decorates a class as IoC injected. No direct instantiation is necessary.
     /// </summary>
     [MeansImplicitUse]
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Enum | AttributeTargets.Interface |
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Enum |
+                    AttributeTargets.Interface |
                     AttributeTargets.Delegate)]
     public class IoCAttribute : Attribute { }
 }
