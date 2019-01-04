1f02b671a22e6b075742649317e8e6437da3be49
Michael A. Volz <mvolz@redmuffin.de>
2019-01-04 16:55:48 +0100

    Add xUnit Fact attribute example

---
+++ tests/BaseTestCode/IgnoreOnAppVeyorLinuxFactAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Xunit;
+
+// ReSharper disable UnusedMember.Global
+
+namespace BaseTestCode
+{
+    public sealed class IgnoreOnAppVeyorLinuxFactAttribute : FactAttribute
+    {
+        /// <summary>
+        ///     Ignore on Linux when run via AppVeyor
+        /// </summary>
+        public IgnoreOnAppVeyorLinuxFactAttribute() {
+            if (IsLinux() && IsAppVeyor()) Skip = "Ignore on Linux when run via AppVeyor";
+        }
+
+        private static bool IsLinux() => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+        private static bool IsAppVeyor() => Environment.GetEnvironmentVariable("APPVEYOR") != null;
+    }
+}
