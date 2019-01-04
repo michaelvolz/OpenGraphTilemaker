3a8f77e8bce56453951ac4aafb0e76fedda01fad
Michael A. Volz <mvolz@redmuffin.de>
2019-01-04 23:34:04 +0100

    Refactor code

--- tests/BaseTestCode/BaseTest.cs
+++ tests/BaseTestCode/BaseTest.cs
@@ -17,32 +17,36 @@ namespace BaseTestCode
     {
         protected BaseTest(ITestOutputHelper output) {
             ConfigureSerilog(output);
-            ConfigureServiceLocator();
+            ConfigureDefaultServices();
+            UpdateServiceLocator();
+
             TestConsole = ApplicationLogging.CreateLogger<T>();
         }
 
-        public ServiceCollection Services { get; } = new ServiceCollection();
+        protected ServiceCollection Services { get; } = new ServiceCollection();
 
         protected ILogger<T> TestConsole { get; }
 
-        protected static HttpClient HttpClient(FakeHttpMessageHandler fakeHttpMessageHandler) => new HttpClient(fakeHttpMessageHandler);
+        protected HttpClient HttpClient(FakeHttpMessageHandler fakeHttpMessageHandler) => new HttpClient(fakeHttpMessageHandler);
 
-        protected static HttpClient HttpClient(string fakeResponse, HttpStatusCode httpStatusCode = HttpStatusCode.OK) {
+        protected HttpClient HttpClient(string fakeResponse, HttpStatusCode httpStatusCode = HttpStatusCode.OK) {
             var message = new HttpResponseMessage(httpStatusCode) { Content = new StringContent(fakeResponse) };
+
             return HttpClient(new FakeHttpMessageHandler(message));
         }
 
-        private static void ConfigureSerilog(ITestOutputHelper output) =>
+        protected void UpdateServiceLocator() => ServiceLocator.SetServiceProvider(Services.BuildServiceProvider());
+
+        private void ConfigureSerilog(ITestOutputHelper output) =>
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Verbose()
                 .WriteTo.TestOutput(output, outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}] [{SourceContext}] {Message}\t{Exception}")
                 .CreateLogger()
                 .ForContext<T>();
 
-        private void ConfigureServiceLocator() {
+        private void ConfigureDefaultServices() {
             Services.AddSingleton(provider => (ILoggerFactory)new SerilogLoggerFactory(null, false));
             Services.TryAdd(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(Logger<>)));
-            ServiceLocator.SetServiceProvider(Services.BuildServiceProvider());
         }
     }
 }
