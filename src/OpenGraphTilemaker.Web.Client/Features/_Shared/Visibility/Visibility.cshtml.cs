a941c1b66d7be6ed25dee9d5245a9f6ea2b9abf7
Michael A. Volz <mvolz@redmuffin.de>
2019-01-04 16:56:51 +0100

    Add visibility control and examples

---
+++ src/OpenGraphTilemaker.Web.Client/Features/_Shared/Visibility/Visibility.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace OpenGraphTilemaker.Web.Client.Features._Shared.Visibility
+{
+    public class VisibilityModel : BlazorComponent
+    {
+        [Parameter] protected RenderFragment ChildContent { get; set; }
+        [Parameter] protected bool Show { get; set; }
+    }
+}
