53681467dc7c1947abd92e0561fa5b6717001865
Michael A. Volz <mvolz@redmuffin.de>
2019-01-05 11:05:28 +0100

    Add IgnoreOnAzurePipelinesFactAttribute

---
+++ tests/BaseTestCode/IgnoreOnAzurePipelinesFactAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace BaseTestCode
+{
+    public sealed class IgnoreOnAzurePipelinesFactAttribute : FactAttribute
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="IgnoreOnAzurePipelinesFactAttribute" /> class.
+        ///     Ignore on Linux when run via AppVeyor
+        /// </summary>
+        public IgnoreOnAzurePipelinesFactAttribute() {
+            if (IsAzurePipelines()) Skip = "Ignore when run via AzurePipelines";
+        }
+
+        private static bool IsAzurePipelines() => Environment.GetEnvironmentVariable("System.TeamFoundationServerUri") != null;
+    }
+}
