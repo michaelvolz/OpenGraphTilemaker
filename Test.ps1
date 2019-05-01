b0e13b232b3971e8fe6ba3ee9a6b453101a47571
Michael A. Volz (Flynn) <mvolz@redmuffin.de>
2019-05-01 13:08:53 +0200

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,13 +1,13 @@
 Get-ChildItem Env:
 
 Push-Location -Path ".\tests\Common.Tests\"
-dotnet test --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
+dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
 Pop-Location
 
 Push-Location -Path ".\tests\OpenGraphTilemaker.Tests\"
-dotnet test --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
+dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
 Pop-Location
 
 Push-Location -Path ".\tests\OpenGraphTilemaker.Web.Client.Tests\"
-dotnet test --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
+dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
 Pop-Location
