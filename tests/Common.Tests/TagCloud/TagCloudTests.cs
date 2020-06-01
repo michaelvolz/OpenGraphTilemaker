cc4c854f0fa047f015a041a212ce01b6bbd2ec9f
Michael A. Volz <mvolz@redmuffin.de>
2020-06-01 20:02:05 +0200

    Fix TagCloud test

--- tests/Common.Tests/TagCloud/TagCloudTests.cs
+++ tests/Common.Tests/TagCloud/TagCloudTests.cs
@@ -10,7 +10,7 @@ namespace Common.Tests.TagCloud
     public class TagCloudTests : BaseTest<TagCloudTests>
     {
         public TagCloudTests(ITestOutputHelper testConsole) : base(testConsole) { }
-        private const string DummyText = "Earthly, of he. parasites! at? so and; for call asp.net of old pomp!!!!!!!! to could that fondly one did hight Earthly .net" + "“My new book is out:";
+        private const string DummyText = "Earthly, of he. parasites! at? so and; for call asp.net of old pomp!!!!!!!! to could that fondly one did hight Earthly .net " + "“My new book is out:";
 
         [Fact]
         public async Task TagCloud_InsertText_ReturnsValidCloud() {
