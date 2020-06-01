6cd54c99adb2b70d9efdca418d3cd424cbd2e871
Michael A. Volz <mvolz@redmuffin.de>
2020-06-01 20:02:15 +0200

    Update test script

--- Test.ps1
+++ Test.ps1
@@ -2,12 +2,15 @@ Get-ChildItem Env:
 
 Push-Location -Path ".\tests\Common.Tests\"
 dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
+if (-not $?) {throw "Failed Common.Tests"}
 Pop-Location
 
 Push-Location -Path ".\tests\OpenGraphTilemaker.Tests\"
 dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
+if (-not $?) {throw "Failed OpenGraphTilemaker.Tests"}
 Pop-Location
 
 Push-Location -Path ".\tests\Experiment.Tests\"
 dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
+if (-not $?) {throw "Failed Experiment.Tests"}
 Pop-Location
