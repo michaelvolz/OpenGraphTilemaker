cc7b928d9fa47da9cfa8c62f5683f6415f2d1a18
Michael A. Volz <mvolz@redmuffin.de>
2020-06-02 21:27:47 +0200

    Minor cleanup

--- src/Experiment/Features/CryptoWatch/DataSource.cs
+++ src/Experiment/Features/CryptoWatch/DataSource.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 using Common.Logging;
 using Microsoft.Extensions.Logging;
@@ -12,19 +11,18 @@ namespace Experiment.Features.CryptoWatch
         public Action<int>? OnUpdate { get; set; }
 
         [MethodImpl(MethodImplOptions.NoOptimization)]
-        public async Task GoAsync() {
+        public async Task GoAsync()
+        {
             var logger = ApplicationLogging.CreateLogger<DataSource>();
 
             logger.LogDebug("DataSource start");
-            for (var i = 0; i < 10000; i++) {
+            for (var i = 0; i < 10000; i++)
+            {
                 logger.LogDebug(i.ToString());
                 OnUpdate?.Invoke(i);
 
                 var x = 0;
                 for (var j = 0; j < 25000; j++) x += 1;
-
-                //Thread.Sleep(1);
-                //await Task.Delay(1);
             }
 
             logger.LogDebug("DataSource end");
