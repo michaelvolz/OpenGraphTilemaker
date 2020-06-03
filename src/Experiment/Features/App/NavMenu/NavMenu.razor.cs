9452d43d527fd52d77c726cd0e6ec11d6de3b25e
Michael A. Volz <mvolz@redmuffin.de>
2020-06-03 13:34:50 +0200

    Update NavMenu

--- src/Experiment/Features/App/NavMenu/NavMenu.razor.cs
+++ src/Experiment/Features/App/NavMenu/NavMenu.razor.cs
@@ -1,24 +1,9 @@
-﻿using System.Threading.Tasks;
-
-namespace Experiment.Features.App.NavMenu
+﻿namespace Experiment.Features.App.NavMenu
 {
     public class NavMenuModel : BlazorComponentStateful<NavMenuModel>
     {
         protected string? NavMenuCSS { get; private set; }
 
         protected void ToggleNavMenu() => NavMenuCSS = NavMenuCSS == null ? "collapsed" : null;
-
-        protected string ServerModeCSS() => IsClientMode() ? string.Empty : "active";
-        protected string ClientModeCSS() => IsClientMode() ? "active" : string.Empty;
-
-        protected async Task ActivateClientMode() =>
-            await JSInteropHelpers.NavigateToAsync(JSRuntime,
-                $"{UriHelper.BaseUri}?mode=client");
-
-        protected async Task ActivateServerMode() =>
-            await JSInteropHelpers.NavigateToAsync(JSRuntime,
-                $"{UriHelper.BaseUri}?mode=server");
-
-        private bool IsClientMode() => false; //JSRuntime.Current is MonoWebAssemblyJSRuntime;
     }
 }
