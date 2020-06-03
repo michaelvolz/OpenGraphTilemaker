585268ae46565b667f2516f73802a9028bd5f714
Michael A. Volz <mvolz@redmuffin.de>
2020-06-03 19:28:25 +0200

    Update test script

--- Test.ps1
+++ Test.ps1
@@ -1,16 +1,18 @@
 Get-ChildItem Env:
 
-Push-Location -Path ".\tests\Common.Tests\"
-dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
-if (-not $?) {throw "Failed Common.Tests"}
-Pop-Location
+dotnet test --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
 
-Push-Location -Path ".\tests\OpenGraphTilemaker.Tests\"
-dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
-if (-not $?) {throw "Failed OpenGraphTilemaker.Tests"}
-Pop-Location
+#Push-Location -Path ".\tests\Common.Tests\"
+#dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
+#if (-not $?) {throw "Failed Common.Tests"}
+#Pop-Location
 
-Push-Location -Path ".\tests\Experiment.Tests\"
-dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
-if (-not $?) {throw "Failed Experiment.Tests"}
-Pop-Location
+#Push-Location -Path ".\tests\OpenGraphTilemaker.Tests\"
+#dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
+#if (-not $?) {throw "Failed OpenGraphTilemaker.Tests"}
+#Pop-Location
+
+#Push-Location -Path ".\tests\Experiment.Tests\"
+#dotnet test --no-build --configuration "Release" --logger "trx;LogFileName=TEST-results.xml"
+#if (-not $?) {throw "Failed Experiment.Tests"}
+#Pop-Location
