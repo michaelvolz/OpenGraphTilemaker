0036026a3ea089d4bc79dd2f4be90f0c598359b9
Michael A. Volz <mvolz@redmuffin.de>
2020-06-14 15:45:30 +0200

    Extract code

---
+++ src/Experiment/Features/Tabs/TabSet.razor.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Components;
+
+namespace Experiment.Features.Tabs
+{
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global", Justification = "Blazor rule")]
+    public partial class TabSet
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; [UsedImplicitly] set; }
+
+        public ITab? ActiveTab { get; private set; }
+
+        public void AddTab(ITab tab)
+        {
+            if (ActiveTab == null) SetActivateTab(tab);
+        }
+
+        public void RemoveTab(ITab tab)
+        {
+            if (ActiveTab == tab) SetActivateTab(null);
+        }
+
+        public void SetActivateTab(ITab? tab)
+        {
+            if (ActiveTab != tab)
+            {
+                ActiveTab = tab;
+                StateHasChanged();
+            }
+        }
+    }
+}
