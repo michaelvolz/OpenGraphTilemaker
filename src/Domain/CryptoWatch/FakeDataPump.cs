3d042ee16850363f9abc199641d4435894e4852e
Michael A. Volz <mvolz@redmuffin.de>
2020-06-14 18:36:34 +0200

    CryptoWatch WIP

---
+++ src/Domain/CryptoWatch/FakeDataPump.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain.CryptoWatch
+{
+    public class FakeDataPump
+    {
+        private readonly Random _random;
+
+        public FakeDataPump() => _random = new Random();
+
+        public async IAsyncEnumerable<CryptoEntity> DataStreamAsync([EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var entity = new CryptoEntity { Name = "Bitcoin", Value = _random.Next(10000), ValueCurrency = "USD" };
+                yield return entity;
+
+                await Task.Delay(_random.Next(100), CancellationToken.None);
+            }
+        }
+    }
+}
---
+++ tests/Domain.Tests/CryptoWatch/FakeDataPumpTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using BaseTestCode;
+using Common.Extensions;
+using Domain.CryptoWatch;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Domain.Tests.CryptoWatch
+{
+    public class FakeDataPumpTests : BaseTest<FakeDataPumpTests>
+    {
+        public FakeDataPumpTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task Get_Some_Data()
+        {
+            var fakeDataPump = new FakeDataPump();
+
+            using var cancellationTokenSource = new CancellationTokenSource(300);
+
+            var cryptoEntities = new List<CryptoEntity>();
+
+            await foreach (var item in fakeDataPump.DataStreamAsync(cancellationTokenSource.Token))
+            {
+                cryptoEntities.Add(item);
+                TestConsole.LogInformation("Item: {Item}", item.ReturnDumpFlat());
+            }
+
+            cryptoEntities.Should().NotBeEmpty();
+            cryptoEntities.Count.Should().BeGreaterThan(0);
+        }
+    }
+}
